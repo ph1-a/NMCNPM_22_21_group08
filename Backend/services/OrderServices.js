8aa0f0fe5431647c3ea467eec00747c6f156a830
ph1-a <godiloveapple@gmail.com>
2025-06-19 06:09:23 +0700

    Add files via upload

--- Backend/services/OrderServices.js
+++ Backend/services/OrderServices.js
@@ -1,15 +1,25 @@
 const OrderRepository = require('../repositories/OrderRepository');
+const MenuItem = require('../models/MenuItem');
 
 module.exports = {
   placeOrder: async (data) => {
-    const { userId, restaurantId, items, address, expectedTotal } = data;
+    const { userId, restaurantId, items, address } = data;
+
+    // If restaurantId is not provided, try to get it from the first menu item
+    let finalRestaurantId = restaurantId;
+    if (!finalRestaurantId && items && items.length > 0) {
+      const firstMenuItem = await MenuItem.findByPk(items[0].foodId);
+      if (firstMenuItem) {
+        finalRestaurantId = firstMenuItem.restaurantId;
+      }
+    }
 
     const orderData = {
       userId,
-      restaurantId,
+      restaurantId: finalRestaurantId, // This can be null now
       address,
       status: 'Pending',
-      total: expectedTotal // Use expectedTotal directly
+      total: 0 // Will be calculated in repository
     };
 
     return await OrderRepository.createOrder(orderData, items);
